be6eac0d8708fd5a1ecca06c23b4ca7d5f31bcd6
Jean <jeanpereira@sisconsultoria.com>
2024-10-31 20:59:01 -0300

    Ajustando layout e API welcome

--- js/formwelcome.js
+++ js/formwelcome.js
@@ -1,28 +1,44 @@
+// const form = document.getElementById('fiap-form');
+// form.addEventListener('submit', (event) => {
+//     console.log('passou')
+function submitForm(){
+    event.preventDefault(); 
 
-alert('ok')
+    // Inputs
+    const student1 = document.getElementById('student1').value;
+    const student2 = document.getElementById('student2').value;
+    const student3 = document.getElementById('student3').value;
+    const student4 = document.getElementById('student4').value;
+    const student5 = document.getElementById('student5').value;
+    const message = document.getElementById('message').value;
 
-    // const url = "https://api.exemplo.com/endpoint"; 
-    // const data = { 
-    // chave1: "valor1",
-    // chave2: "valor2"
-    // };
+    // Criando um objeto com os dados a serem enviados
+    const data = {
+        "names": [
+            student1, student2, student3, student4, student5
+        ],
+        message
+    }
+    const url = "https://fsdt-contact.onrender.com/contact";
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro na requisição");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Resposta:", data);
+    })    
+    .catch(error => {
+        console.error("Erro:", error);
+    });
+}
+// });
 
-    // fetch(url, {
-    // method: "POST",
-    // headers: {
-    //     "Content-Type": "application/json"
-    // },
-    // body: JSON.stringify(data)
-    // })
-    // .then(response => {
-    //     if (!response.ok) {
-    //     throw new Error("Erro na requisição");
-    //     }
-    //     return response.json();
-    // })
-    // .then(data => {
-    //     console.log("Resposta:", data);
-    // })
-    // .catch(error => {
-    //     console.error("Erro:", error);
-    // });
