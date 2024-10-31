14678096d9087def56a6e73a8d3c76ab3dd09e22
Jean <jeanpereira@sisconsultoria.com>
2024-10-31 20:15:08 -0300

    first commit

---
+++ js/formwelcome.js
@@ -0,0 +1,28 @@
+
+alert('ok')
+
+    // const url = "https://api.exemplo.com/endpoint"; 
+    // const data = { 
+    // chave1: "valor1",
+    // chave2: "valor2"
+    // };
+
+    // fetch(url, {
+    // method: "POST",
+    // headers: {
+    //     "Content-Type": "application/json"
+    // },
+    // body: JSON.stringify(data)
+    // })
+    // .then(response => {
+    //     if (!response.ok) {
+    //     throw new Error("Erro na requisição");
+    //     }
+    //     return response.json();
+    // })
+    // .then(data => {
+    //     console.log("Resposta:", data);
+    // })
+    // .catch(error => {
+    //     console.error("Erro:", error);
+    // });
